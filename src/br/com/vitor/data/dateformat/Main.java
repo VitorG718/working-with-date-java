84882d1ad5a8e57f7f28b556f356e49109a26cf1
VitorG718 <vitorx1280@gmail.com>
2021-01-25 16:47:16 -0300

    Added DateFormat methods

---
+++ src/br/com/vitor/data/dateformat/Main.java
@@ -0,0 +1,30 @@
+package br.com.vitor.data.dateformat;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("0 - " + now);
+
+        String dateStr = DateFormat.getInstance().format(now);
+        System.out.println("1 - " + dateStr);
+
+        dateStr = DateFormat.getDateTimeInstance().format(now);
+        System.out.println("2 - " + dateStr);
+
+        dateStr = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(now);
+        System.out.println("3 - " + dateStr);
+
+        dateStr = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT).format(now);
+        System.out.println("4 - " + dateStr);
+
+        dateStr = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.LONG).format(now);
+        System.out.println("5 - " + dateStr);
+
+        dateStr = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG).format(now);
+        System.out.println("6 - " + dateStr);
+    }
+}
